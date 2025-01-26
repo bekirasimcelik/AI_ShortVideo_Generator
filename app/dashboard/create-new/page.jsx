20d75f03ce274bde59fffbb02262ae1af427af24
bekirasimcelik <bekirasimcelik@hotmail.com>
2025-01-26 15:27:06 +0300

    create-new flow

--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import SelectTopic from "./_components/SelectTopic";
 
 function CreateNew() {
-  return <div>CreateNew</div>;
+  return (
+    <div className="md:px-29">
+      <h2 className="font-bold text-4xl text-primary text-center">
+        Create New
+      </h2>
+
+      <div className="mt-10 shadow-md p-10">
+        {/* Select Topic */}
+        <SelectTopic />
+
+        {/* Select Style */}
+
+        {/* Duration */}
+
+        {/* Create */}
+      </div>
+    </div>
+  );
 }
 
 export default CreateNew;
