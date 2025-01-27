654bf3b422f1b7dc96aecc6dd48305c0b1b362ee
bekirasimcelik <bekirasimcelik@hotmail.com>
2025-01-27 11:30:07 +0300

    select style flow

--- app/dashboard/create-new/_components/SelectStyle.jsx
+++ app/dashboard/create-new/_components/SelectStyle.jsx
@@ -1,13 +1,47 @@
+import Image from "next/image";
 import React from "react";
 
 function SelectStyle() {
   const styleOptions = [
     {
       name: "Realistic",
-      image: "real.png",
+      image: "/real.png",
+    },
+    {
+      name: "Cartoon",
+      image: "/cartoon.png",
+    },
+    {
+      name: "Comic",
+      image: "/cartoon.png",
+    },
+    {
+      name: "WaterColor",
+      image: "/cartoon.png",
+    },
+    {
+      name: "GTA",
+      image: "/cartoon.png",
     },
   ];
-  return <div>SelectStyle</div>;
+  return (
+    <div className="mt-7">
+      <h2 className="font-bold text-2xl text-primary">Style</h2>
+      <p className="text-gray-500">Select your video style</p>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+        {styleOptions.map((item, index) => (
+          <div>
+            <Image
+              src={item.image}
+              width={100}
+              height={100}
+              className="h-48 object-cover rounded-lg w-full"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default SelectStyle;
