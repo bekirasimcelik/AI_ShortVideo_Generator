11f95fa6759207564cd8ef80340096f0afedc1c8
bekirasimcelik <bekirasimcelik@hotmail.com>
2025-02-01 00:59:38 +0300

    camera zoom implemented

--- app/dashboard/_components/RemotionVideo.jsx
+++ app/dashboard/_components/RemotionVideo.jsx
@@ -3,6 +3,7 @@ import {
   AbsoluteFill,
   Audio,
   Img,
+  interpolate,
   Sequence,
   useCurrentFrame,
   useVideoConfig,
@@ -40,35 +41,49 @@ function RemotionVideo({
           backgroundColor: "black",
         }}
       >
-        {imageList?.map((item, index) => (
-          <Sequence
-            key={index}
-            from={(index * getDurationFrame()) / imageList?.length}
-            durationInFrames={getDurationFrame()}
-          >
-            <AbsoluteFill
-              style={{ justifyContent: "center", alignItems: "center" }}
+        {imageList?.map((item, index) => 
+        {
+          const startTime = (index * getDurationFrame()) / imageList?.length;
+          const duration = getDurationFrame();
+
+          const scale = (index) => interpolate(
+            frame,
+            [startTime, startTime + duration/2, startTime + duration], // zoom in out
+            index %2 ==0 ? [1, 1.8, 1] : [1.8,1,1.8],
+            {extrapolateLeft: 'clamp', extrapolateRight: 'clamp'}
+          )
+
+          return (
+          <>  
+            <Sequence
+              key={index}
+              from={startTime}
+              durationInFrames={getDurationFrame()}
             >
-              <Img
-                src={item}
-                style={{ width: "100%", height: "100%", objectFit: "cover" }}
-              />
               <AbsoluteFill
-                style={{
-                  color: "white",
-                  justifyContent: "center",
-                  top: undefined,
-                  bottom: 50,
-                  height: 150,
-                  textAlign: "center",
-                  width: "100%",
-                }}
+                style={{ justifyContent: "center", alignItems: "center" }}
               >
-                <h2 className="text-2xl">{getCurrentCaptions()}</h2>
+                <Img
+                  src={item}
+                  style={{ width: "100%", height: "100%", objectFit: "cover", transform: `scale(${scale(index)})` }}
+                />
+                <AbsoluteFill
+                  style={{
+                    color: "white",
+                    justifyContent: "center",
+                    top: undefined,
+                    bottom: 50,
+                    height: 150,
+                    textAlign: "center",
+                    width: "100%",
+                  }}
+                >
+                  <h2 className="text-2xl">{getCurrentCaptions()}</h2>
+                </AbsoluteFill>
               </AbsoluteFill>
-            </AbsoluteFill>
-          </Sequence>
-        ))}
+            </Sequence>
+          </>
+        )})}
         <Audio src={audioFileUrl} />
       </AbsoluteFill>
     )
