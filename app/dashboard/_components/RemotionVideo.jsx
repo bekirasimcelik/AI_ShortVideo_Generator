8e7c0e0e11bf7a5426f0f7ed1da4c24ac6171a18
bekirasimcelik <bekirasimcelik@hotmail.com>
2025-01-31 23:37:17 +0300

    captions added

--- app/dashboard/_components/RemotionVideo.jsx
+++ app/dashboard/_components/RemotionVideo.jsx
@@ -25,10 +25,16 @@ function RemotionVideo({ script, imageList, audioFileUrl, captions, setDurationI
             from={((index * getDurationFrame()) / imageList?.length)}
             durationInFrames={getDurationFrame()}
           >
+          <AbsoluteFill style={{justifyContent:'center', alignItems:'center'}}>
             <Img
               src={item}
               style={{ width: "100%", height: "100%", objectFit: "cover" }}
             />
+            <AbsoluteFill style={{color:"white", justifyContent:'center', top: undefined, bottom: 50, height: 150, textAlign: 'center', width: '100%'}}>
+              <h2 className="text-2xl">Captions</h2>
+            </AbsoluteFill>
+          </AbsoluteFill>
+
           </Sequence>
         </>
       ))}
